a27fa0af12b5d32b096c45f987134af227226ea8
Jonathan Park <jgpark2@illinois.edu>
2015-03-09 13:22:28 -0500

    nodes have key val dict, config parse for central server

---
+++ CS425_MP1/src/mp1/Datum.java
@@ -0,0 +1,23 @@
+package mp1;
+
+public class Datum {
+
+	public String value;
+	public long timestamp;
+	public long timestamp2; //maybe
+	
+	public Datum(String val, long ts) {
+		value = val;
+		timestamp = ts;
+	}
+	
+	public Datum(String val, long ts, long ts2) {
+		value = val;
+		timestamp = ts;
+		timestamp2 = ts2;
+	}
+	
+	public void addAckTimestamp(long ts2) {
+		timestamp2 = ts2;
+	}
+}
